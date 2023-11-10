7bf48994cc1ba8c289161b6cd8f87a683b39b190
zv8001 <z3ck200@gmail.com>
2023-11-10 12:58:01 -0600

    v1.0

---
+++ mp4toamv/Form1.Designer.vb
@@ -0,0 +1,48 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        OpenFileDialog1 = New OpenFileDialog()
+        SaveFileDialog1 = New SaveFileDialog()
+        SuspendLayout()
+        ' 
+        ' OpenFileDialog1
+        ' 
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(386, 102)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        ShowInTaskbar = False
+        Text = "Form1"
+        WindowState = FormWindowState.Minimized
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
---
+++ mp4toamv/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Made by zv800 © 2023 freeware tool. (works with mp3 players)", 64 + 0, "mp4toamv https://www.zv800.com")
+        OpenFileDialog1.Title = "Please select a mp4 file"
+        OpenFileDialog1.InitialDirectory = "C:\"
+        OpenFileDialog1.Filter = "mp4 Files|*.mp4"
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            SaveFileDialog1.Filter = "amv Files (*.amv*)|*.amv"
+            If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK _
+       Then
+                MyUtilities.RunCommandCom("ffmpeg.dll -i """ & OpenFileDialog1.FileName & """ -c:v amv -c:a adpcm_ima_amv -pix_fmt yuvj420p -vstrict -1 -s 160x120 -ac 1 -ar 22050 -r 25 -block_size 882 """ & SaveFileDialog1.FileName & """", "/W", True)
+            Else
+                Me.Close()
+            End If
+        Else
+            Me.Close()
+        End If
+
+    End Sub
+    Public Class MyUtilities
+        Shared Sub RunCommandCom(command As String, arguments As String, permanent As Boolean)
+            Dim p As Process = New Process()
+            Dim pi As ProcessStartInfo = New ProcessStartInfo()
+            pi.Arguments = " " + If(permanent = True, "/K", "/C") + " " + command
+            pi.FileName = "cmd.exe"
+            p.StartInfo = pi
+            p.Start()
+        End Sub
+    End Class
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+
+    End Sub
+End Class
